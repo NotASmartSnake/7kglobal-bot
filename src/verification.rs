230cf62f993d1685442f083bc8dff39570c81fb9
Nathan Young <nathantayloryoung@gmail.com>
2025-09-15 21:09:07 +1200

    Create role when role is not found

--- src/verification.rs
+++ src/verification.rs
@@ -1,4 +1,4 @@
-use serenity::builder::{CreateEmbed, EditMember, EditMessage};
+use serenity::builder::{CreateEmbed, EditMember, EditMessage, EditRole};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
@@ -48,19 +48,21 @@ impl VerificationInfo {
             emoji_shortcode = exception;
         }
 
-        let role = match guild.role_by_name(
-            &(country.to_string()
-                + " "
-                + emojis::get_by_shortcode(emoji_shortcode)
-                    .ok_or(format!("Could not get emoji from country: {}", &country))?
-                    .as_str()),
-        ) {
+        let role_name = country.to_string()
+            + " "
+            + emojis::get_by_shortcode(emoji_shortcode)
+                .ok_or(format!("Could not get emoji from country: {}", &country))?
+                .as_str();
+
+        let role = match guild.role_by_name(&role_name) {
             Some(role) => role,
             None => {
-                return Err(format!(
-                    "There is currently no role set up for the country: {}",
-                    &country
-                ));
+                // create role if it doesn't already exist
+                let role_builder = EditRole::new().name(&role_name);
+                &guild
+                    .create_role(&ctx.http, role_builder)
+                    .await
+                    .map_err(|_| format!("Could not create new role: {role_name}"))?
             }
         };
 
